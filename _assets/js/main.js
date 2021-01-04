ba7202491a4e7a028e5e519a9890dbd867b8f760
Daniel <Daniel>
2021-01-03 20:35:13 -0800

    Initital check of my-blog

---
+++ _assets/js/main.js
@@ -0,0 +1,12 @@
+// Bootstrap JS and CSS
+import 'bootstrap';
+import './../css/main.scss';
+
+// Bootstrap Popover
+import './vendor/popover';
+
+// Bootstrap Tooltip
+import './vendor/tooltip';
+
+//Fontawesome
+import './vendor/fontawesome';
---
+++ _assets/js/vendor/fontawesome.js
@@ -0,0 +1,5 @@
+
+import "../../../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js"
+import "../../../node_modules/@fortawesome/fontawesome-free/js/regular.js"
+import "../../../node_modules/@fortawesome/fontawesome-free/js/solid.js"
+import "../../../node_modules/@fortawesome/fontawesome-free/js/brands.js"
